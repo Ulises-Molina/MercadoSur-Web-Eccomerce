ed9cc652817a4b89ce0fd843c5d0dd1883e0bfd8
Ulises Molina <uli.molina10@gmail.com>
2024-12-06 10:22:17 -0300

    Agregue framer para animaciones

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useContext, React } from 'react';
 import { ProductosContext } from '../context/ProductosContext';
 import { DarkModeContext } from '../context/DarkModeContext';
+import {motion} from "motion/react"
 
 
 export const Card = ({imagen,nombre,precio,id}) => {
@@ -21,7 +22,11 @@ export const Card = ({imagen,nombre,precio,id}) => {
 
     return (
         <>
-            <div className={darkMode ? "tarjeta-dark-mode" : "tarjeta"} onClick={manejarClick}>
+            <motion.div 
+            initial={{opacity: 0, translateY: 130}}
+            whileInView={{opacity: 1, translateY: 0,transition: {duration: 0.6}}}
+            whileHover={{translateY: -20}}
+            className={darkMode ? "tarjeta-dark-mode" : "tarjeta"} onClick={manejarClick}>
                 <div className='img-container'>
                 <img className={darkMode ? "img-tarjeta-dark-mode" :'img-tarjeta'} src={imagen} alt={nombre}loading='lazy'></img>
                 </div>
@@ -29,7 +34,7 @@ export const Card = ({imagen,nombre,precio,id}) => {
                 <h2 className='description'>{nombre}</h2>
                 <p className='price'>$ {precio}</p>
                 </div>
-            </div>
+            </motion.div>
         </>
     )
 }
